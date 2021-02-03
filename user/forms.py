6864cdd807d27b0da7d79c9466aefb3cef6d0712
Prashant1099 <patelprashant356@gmail.com>
2021-02-04 03:47:27 +0530

    Made email address a unique constraint

--- user/forms.py
+++ user/forms.py
@@ -10,5 +10,12 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2',]
+        fields = ['username', 'email', 'password1', 'password2', 'check_box']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError(u'A user with that email address already exists.')
+        return email
     
