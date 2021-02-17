5c0723d35bf909eb9fe95a7cee866d1b522f2eac
Prashant1099 <patelprashant356@gmail.com>
2021-02-18 03:22:53 +0530

    Added Pagination in Course Dashboard

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import *
-from django.db.models import Q
+# from django.db.models import Q
+from django.core.paginator import Paginator
 
 def all_courses(request):
     courses = Course.objects.all()
     lessons = Lessons.objects.all()
+    course_categoies = CourseCategory.objects.all()
+    memberships = Membership.objects.all()
+
+    # Pagination
+    paginator = Paginator(courses, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     # if request.method == "GET":
     #     searched_course = request.GET.get('q')
@@ -13,6 +21,9 @@ def all_courses(request):
     context = {
         'courses': courses,
         'lessons': lessons,
+        'page_obj': page_obj,
+        'course_categoies': course_categoies,
+        'memberships': memberships,
         # 'course_list': course_list,
     }
 
