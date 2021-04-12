3caf5f0668e036fbe31dbc1d9e49521cf8554a9c
Prashant1099 <patelprashant356@gmail.com>
2021-04-12 14:03:29 +0530

    Made changes for Heroku Hosting

--- Thinker_Lab/urls.py
+++ Thinker_Lab/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# for Heroku Hosting
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -26,6 +30,10 @@ urlpatterns = [
     path('', include('courses.urls')),
     path('', include('coding.urls')),
     path('', include('quiz.urls')),
+
+    # fro Heroku Hosting
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
